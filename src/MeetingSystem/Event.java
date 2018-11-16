c2d77950ee510243836c41e26789e68716bc0bb6
Wojciech Wojcik <wojtek.wroclaw@hotmail.com>
2018-11-16 14:36:07 +0100

    init commit

---
+++ src/MeetingSystem/Event.java
@@ -0,0 +1,14 @@
+package MeetingSystem;
+
+import java.util.Date;
+
+public class Event {
+
+    String name;
+    String theme;
+    Date time;
+    Place place;
+
+
+
+}
---
+++ src/MeetingSystem/Place.java
@@ -0,0 +1,5 @@
+package MeetingSystem;
+
+public class Place {
+
+}
